fcd8a63ce293ce5e4e288c804c348a8545e2298e
felipelyra3 <felipelyra3@gmail.com>
2022-10-19 09:29:58 -0300

    Feat SignIn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,92 @@
 import React from "react";
+import axios from "axios";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef, useContext } from "react";
+import UserContext from "../../Contexts/UserContext"
+import { HiUserCircle } from "react-icons/hi";
 
 export default function Header() {
+  const img = localStorage.getItem("img");
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const context = useContext(UserContext);
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handler);
+  }, []);
+
+  function SignOut() {
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    const signout = axios.delete(context.BASE_URL + '/signout', config);
+
+    signout.then(() => {
+      localStorage.clear();
+      navigate('/');
+    });
+
+    signout.catch((error) => {
+      console.log(error);
+      localStorage.clear();
+      navigate('/');
+    });
+  }
+
+  function SignOutAll() {
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    const signoutall = axios.delete(context.BASE_URL + '/signoutall', config);
+
+    signoutall.then(() => {
+      localStorage.clear();
+      navigate('/');
+    });
+
+    signoutall.catch((error) => {
+      console.log(error);
+      localStorage.clear();
+      navigate('/');
+    });
+  }
+
   return (
     <Wraped>
       <div>
         <h1>Linkr</h1>
-        <img src="" alt=".." />
+        {/* <img src="" alt=".." /> */}
+        <Dropdown ref={menuRef}>
+          <DropdownTrigger onClick={() => { setOpen(!open) }}>
+            {img === 'null' ? <ContainerHiUserCircle><HiUserCircle /></ContainerHiUserCircle> : <img src={img} alt="" />}
+          </DropdownTrigger>
+
+          <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
+            <nav>
+              <ul>
+                <li onClick={() => { SignOut() }}>SignOut</li>
+                <li onClick={() => { SignOutAll() }}>SignOut All</li>
+              </ul>
+            </nav>
+          </div>
+        </Dropdown>
+
+
+
       </div>
     </Wraped>
   );
@@ -40,5 +120,74 @@ const Wraped = styled.header`
     background-color: #ffffff;
     border-radius: 50%;
     font-size: 32px;
+    width: 53px;
+    height: 53px;
   }
 `;
+
+const ContainerHiUserCircle = styled(HiUserCircle)`
+  transform: scale(3);
+  margin-right: 24px;
+`;
+
+const Dropdown = styled.div`
+  ul{
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .dropdown-menu.active {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+    transition: var(500ms) ease;
+  }
+
+  .dropdown-menu.inactive {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(-20px);
+    transition: var(500ms) ease;
+  }
+
+  .dropdown-menu {
+    position: absolute;
+    top: 70px;
+    right: -70px;
+    background-color: #151515;
+    border-radius: 0px 0px 20px 20px;
+    padding: 10px 20px;
+    width: 200px;
+
+  :before {
+    content: '';
+    position: absolute;
+    top: -5px;
+    right: 20px;
+    height: 20px;
+    width: 20px;
+    transform: rotate(45deg);
+  }
+
+  li {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    color: #FFFFFF;
+    padding: 8px 0px 8px 0px;
+  }
+
+  ul li:hover {
+    color: #E5E5E5;
+    cursor: pointer;
+  }
+  }
+`;
+
+const DropdownTrigger = styled.div``;
--- src/components/Logged/Logged.js
+++ src/components/Logged/Logged.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Header from "./components/Header/Header.js";
+import Header from "../Header/Header";
 
 export default function Logged() {
     return (
