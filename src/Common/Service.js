9f8cfdf096fec0ed5abc0410b5a4103898206344
savioferraz <savio.ferraz88@gmail.com>
2022-10-21 18:53:16 -0300

    feat: add delete modal

--- src/Common/Service.js
+++ src/Common/Service.js
@@ -6,44 +6,64 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function createHeaders() {
+  const auth = JSON.parse(localStorage.getItem("token"));
+  const config = {
+    headers: {
+      Authorization: `Bearer ${auth.token}`,
+    },
+  };
+  return config;
+}
 
-    const auth = JSON.parse(localStorage.getItem("token"));
-    const config = {
-      headers: {
-        Authorization: `Bearer ${auth.token}`
-      }
-    };
-    return config;
-};
-
-function getUser (id) {
-    const config = createHeaders()
-    const request = axios.get(`${BASE_URL}/user/${id}`, config);
-    return request;
-};
+function getUser(id) {
+  const config = createHeaders();
+  const request = axios.get(`${BASE_URL}/user/${id}`, config);
+  return request;
+}
 
 function getUsers() {
-    const config = createHeaders()
-    const request = axios.get(`${BASE_URL}/users`, config);
-    return request;
-};
+  const config = createHeaders();
+  const request = axios.get(`${BASE_URL}/users`, config);
+  return request;
+}
 
 function searchUser(query) {
-    const config = createHeaders()
-    const request = axios.get(`${BASE_URL}/users?keyword=${query}`, config);
-    return request;
+  const config = createHeaders();
+  const request = axios.get(`${BASE_URL}/users?keyword=${query}`, config);
+  return request;
+}
+
+function postUrls(body) {
+  const config = createHeaders();
+  const request = axios.post(`${BASE_URL}/post`, body, config);
+  return request;
+}
+
+function getPosts() {
+  const config = createHeaders();
+  const request = axios.get(`${BASE_URL}/timeline`, config);
+  return request;
+}
+
+function editPost(id, body) {
+  const config = createHeaders();
+  const request = axios.put(`${BASE_URL}/posts/${id}`, body, config);
+  return request;
+}
+
+function deletePost(id) {
+  const config = createHeaders();
+  const request = axios.delete(`${BASE_URL}/posts/${id}`, config);
+  return request;
+}
+
+export {
+  createHeaders,
+  getUser,
+  getUsers,
+  searchUser,
+  postUrls,
+  getPosts,
+  editPost,
+  deletePost,
 };
-
-function postUrls(body){
-  const config = createHeaders();
-  const request = axios.post(`${BASE_URL}/post`,body,config);
-  return request;
-}
-
-function getPosts(){
-  const config = createHeaders();
-  const request = axios.get(`${BASE_URL}/timeline`,config);
-  return request;
-}
-
-export { createHeaders, getUser, getUsers, searchUser, postUrls, getPosts };
--- src/components/UserProfile/Profile.js
+++ src/components/UserProfile/Profile.js
@@ -1,94 +1,97 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { getUser } from "../../Common/Service";
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 import Posts from "./Posts";
 import Loader from "../../Common/Loader";
+import DeleteModal from "../Modals/DeleteModal";
 
-export default function UserProfile () {
+export default function UserProfile() {
+  const params = useParams();
+  const navigate = useNavigate();
 
-    const params = useParams();
-    const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const [user, setUser] = useState([]);
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const promise = getUser(params.id);
+    promise.then((res) => {
+      setUser(res.data);
+      console.log(user);
 
-    useEffect(() => {
-        const promise = getUser(params.id);
-        promise.then((res => {
+      if (res.data.userPosts) {
+        setPosts(res.data.userPosts);
+      }
+    });
 
-            setUser(res.data);
-            console.log(user)
+    promise.catch(() => {
+      navigate("/404");
+    });
+  }, [params.id]);
 
-            if (res.data.userPosts) {
-                setPosts(res.data.userPosts);
-            }
-            
-        }));
-
-        promise.catch(() => {
-            navigate("/404");
-        });
-
-    }, [params.id]);
-
-    return (
-        <> 
-            {user.length === 0 ? (
-                <LeftSection>
-                    <Loader />
-                </LeftSection>
-            ):(
-                <LeftSection>
-                    <UserInfos>
-                        <ProfilePicture img = {user.img}/>
-                        <Title>
-                            {user.name}'s posts
-                        </Title>
-                    </UserInfos>
-                    <Posts 
-                        posts = {posts} 
-                        img = {user.img}
-                        name = {user.name}
-                    />
-                </LeftSection>
-            )}
-        </>
-    )
-};
+  return (
+    <>
+      {modalOpen ? (
+        <DeleteModal closeModal={() => setModalOpen(false)} />
+      ) : (
+        <></>
+      )}
+      {user.length === 0 ? (
+        <LeftSection>
+          <Loader />
+        </LeftSection>
+      ) : (
+        <LeftSection>
+          <UserInfos>
+            <ProfilePicture img={user.img} />
+            <Title>{user.name}'s posts</Title>
+          </UserInfos>
+          <Posts
+            id={posts.id}
+            posts={posts}
+            img={user.img}
+            name={user.name}
+            delClick={() => setModalOpen(true)}
+          />
+        </LeftSection>
+      )}
+    </>
+  );
+}
 
 const LeftSection = styled.div`
-    padding-top: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const UserInfos = styled.div`
-    width: 661px;
-    padding: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-`
+  width: 661px;
+  padding: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
 
 const ProfilePicture = styled.div`
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    background-color: red;
-    background-size: cover;
-    background-position: center;
-    background-image: ${props => `url(${props.img})`};
-`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: red;
+  background-size: cover;
+  background-position: center;
+  background-image: ${(props) => `url(${props.img})`};
+`;
 
 const Title = styled.h1`
-    font-family: 'Oswald';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 43px;
-    line-height: 64px;
-    color: #FFFFFF;
+  font-family: "Oswald";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 43px;
+  line-height: 64px;
+  color: #ffffff;
 
-    margin-left: 20px;
-`
+  margin-left: 20px;
+`;
