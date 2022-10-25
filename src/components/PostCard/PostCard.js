7c0b6b4bcf6e4645c2074a444705f14a37b8f428
felipelyra3 <felipelyra3@gmail.com>
2022-10-25 13:14:45 -0300

    Feat: Get and Post comments

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -5,8 +5,9 @@ import {
   HeartLiked,
   ContainerHiCircle,
 } from "../../Styles/Icons";
-import { HiTrash, HiPencil } from "react-icons/hi";
+import { HiTrash, HiPencil, HiUserCircle } from "react-icons/hi";
 import { AiOutlineComment } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
 import { BiRepost } from "react-icons/bi";
 import Microlink from "@microlink/react";
 import styled from "styled-components";
@@ -17,6 +18,30 @@ import { deletePost, postLikes } from "../../Common/Service";
 import EditDescriptionInput from "../Inputs/EditDescriptionInput";
 import Description from "../Description/Description";
 import ReactTooltip from "react-tooltip";
+import axios from "axios";
+
+function CommentJSX({ index, userId, name, img, comment }) {
+  return (
+    <>
+      <div className="containerAvatarComment">
+        <div className="avatar">{img === null ? <ContainerHiUserCircle /> : <img src={img} alt="avatar" />}</div>
+
+        <div className="containerNameComment">
+          <div className="name">
+            <h1>{name}</h1>
+            <h2>• following</h2>
+          </div>
+
+          <div className="comment">
+            <h1>{comment}</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="grayLine"></div>
+    </>
+  );
+};
 
 export default function PostCard({
   postId,
@@ -40,6 +65,12 @@ export default function PostCard({
     likeArray.find((obj) => obj.userId === userId)
   );
   const [likeMessage, setLikeMessage] = useState("");
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
+  const [commentsNumber, setCommentsNumber] = useState(0);
+  const [commentBox, setCommentBox] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+
   useEffect(() => {
     const noUser = likeArray.filter((obj) => obj.userId !== userId);
     if (liked) {
@@ -97,6 +128,65 @@ export default function PostCard({
       .catch(() => alert("Error when sending like, try again later"));
   }
 
+  //handleComments();
+  useEffect(() => {
+    handleComments();
+  }, [refresh]);
+
+  function handleComments() {
+    let token = localStorage.getItem("token");
+    token = JSON.parse(token);
+    token = token.token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    const getComments = axios.get(process.env.REACT_APP_API_BASE_URL + '/comments/' + postId, config);
+
+    getComments.then((answer) => {
+      setComments(answer.data);
+      setCommentsNumber((answer.data).length);
+    });
+
+    getComments.catch((error) => {
+      console.log(error);
+    });
+  }
+
+  function sendComment(e) {
+    e.preventDefault();
+
+    let userId = localStorage.getItem("linkr-userId");
+    userId = Number(userId);
+    const body = {
+      userId: userId,
+      postId: postId,
+      comment: comment
+    }
+
+    let token = localStorage.getItem("token");
+    token = JSON.parse(token);
+    token = token.token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    const postComment = axios.post(process.env.REACT_APP_API_BASE_URL + '/comment', body, config);
+
+    postComment.then(() => {
+      setComment('');
+      setRefresh(!refresh);
+    });
+
+    postComment.catch((error) => {
+      console.log(error);
+    });
+  };
+
   return (
     <>
       {modalOpen ? (
@@ -132,10 +222,10 @@ export default function PostCard({
             <ReactTooltip place="left" />
           </span>
 
-          <Comments onClick={() => { console.log('aaaaaaa') }}>
+          <Comments onClick={() => { setCommentBox(!commentBox) }}>
             <AiOutlineComment />
           </Comments>
-          <Text><h1>X {/* comments */}</h1></Text>
+          <Text><h1>{commentsNumber} {/* comments */}</h1></Text>
 
 
           <Reposts onClick={() => { console.log('aaaaaaa') }}>
@@ -191,6 +281,22 @@ export default function PostCard({
           <Links url={url} target="_blank" />
         </div>
       </PostBox>
+
+      <CommentsDropDown>
+        <div className={`dropdown-menu ${commentBox ? 'active' : 'inactive'}`}>
+          <CommentBox>
+            {comments.map((comment, key) => <CommentJSX key={key} index={key + 1} userId={comment.userId} name={comment.name} img={comment.img} comment={comment.comment} />)}
+
+            <FormComment onSubmit={sendComment}>
+              {/* {img === null ? <ContainerHiUserCircle /> : <img src={img} alt="avatar" />} */}
+              {img === localStorage.getItem("img") ? <ContainerHiUserCircle /> : <img src={localStorage.getItem("img")} alt='' />}
+              <input type="text" id="comment" placeholder="Write a comment..." value={comment} onChange={(e) => { setComment(e.target.value) }}></input>
+              <button type="submit" className="button"><ContainerFiSend /></button>
+            </FormComment>
+          </CommentBox>
+        </div>
+      </CommentsDropDown>
+
     </>
   );
 }
@@ -222,6 +328,138 @@ const Text = styled.span`
   text-align: center;
   color: #FFFFFF;
   margin-top: 12px;
+`;
+
+const CommentsDropDown = styled.div`
+  //z-index: -1;
+
+  .dropdown-menu.active {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+    transition: var(500ms) ease;
+  }
+
+  .dropdown-menu.inactive {
+    opacity: 0;
+    visibility: hidden;
+    display: none;
+    transform: translateY(-20px);
+    transition: var(500ms) ease;
+    margin-top: 0px;
+  }
+`;
+
+const CommentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 611px;
+  background: #1E1E1E;
+  border-radius: 16px;
+  margin: -50px 0px 24px 0px;
+  padding: 50px 0px 24px 24px;
+
+    .containerAvatarComment {
+      display: flex;
+      margin-top: 12px;
+    }
+
+    img {
+      width: 39px;
+      height: 39px;
+      border-radius: 26.5px;
+    }
+
+
+    .containerNameComment{
+      margin-left: 18px;
+    }
+
+    .name {
+      display: flex;
+    }
+
+    .name h1 {
+      font-family: 'Lato';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 17px;
+      color: #F3F3F3;
+      margin-right: 4px;
+    }
+
+    .name h2 {
+      font-family: 'Lato';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 17px;
+      color: #565656;
+    }
+
+    .comment h1{
+      font-family: 'Lato';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 17px;
+      color: #ACACAC;
+      margin-top: 4px;
+    }
+
+    .grayLine {
+      width: 571px;
+      height: 1px;
+      border: 1px solid #353535;
+      transform: rotate(-0.1deg);
+      margin-top: 18px;
+    }
+`;
+
+const FormComment = styled.form`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 18px;
+  //z-index: 1;
+
+    input[type="text"] {
+      width: 510px;
+      height: 39px;
+      background: #252525;
+      border-radius: 8px;
+      border: none;
+      margin-left: 12px;
+
+      font-family: 'Lato';
+      font-style: italic;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0.05em;
+      color: #575757;
+      padding-left: 12px;
+    }
+
+    .button {
+      //visibility: hidden;
+      border: 0; 
+      background: transparent;
+    }
+
+`;
+
+const ContainerHiUserCircle = styled(HiUserCircle)`
+  width: 39px;
+  height: 39px;
+`;
+
+const ContainerFiSend = styled(FiSend)`
+  width: 24px;
+  height: 24px;
+  color: #F3F3F3;
+  margin-left: -50px;
 `;
 
 const Reposts = styled(BiRepost)`
